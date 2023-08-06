947f4581d6da1053bdf3e2e643baf13af774dd5a
Tushar Singh <tushar.singh.sengar@gmail.com>
2023-08-06 12:37:18 +0530

    if user click on massage icon a popup come in full screen is completed succesfully :)

--- script.js
+++ script.js
@@ -1,17 +1,45 @@
+
+//global variable
+
+
+
+
 var likeBtn = document.querySelector("#notification");
 var msg = document.querySelector("#msg");
 
+var msgClose = document.querySelector("#msgClose");
+
+
+
+// notification 
 
 
 
 likeBtn.addEventListener("click", ()=>{
     let notfyBox = document.querySelector('.notification-box');
-    notfyBox.classList.toggle("show-notfy-box")
-})
+    notfyBox.classList.toggle("show-notfy-box");
+});
 
 
-//a new section of script 
+
+// massages open
+
+
+
 
 msg.addEventListener("click",()=>{
-    alert(1)
-})
+    document.querySelector(".msg-inbox").classList.add('show-msg-inbox');
+    
+});
+
+
+
+
+// massages close
+
+
+
+msgClose.addEventListener("click",()=>{
+    document.querySelector(".msg-inbox").classList.remove('show-msg-inbox');
+    
+});
