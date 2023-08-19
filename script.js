6fb5f6a4f8a2869ab59ea1820993da15e5b7f9d3
Tushar Singh <tushar.singh.sengar@gmail.com>
2023-08-19 16:04:09 +0530

    New code for notification (if user click outside close the notification box)

--- script.js
+++ script.js
@@ -13,14 +13,29 @@ var msgClose = document.querySelector("#msgClose");
 
 // notification 
 
+/*
 
-
-likeBtn.addEventListener("click", ()=>{
+likeBtn.addEventListener("mouseup",()=>{
     let notfyBox = document.querySelector('.notification-box');
-    notfyBox.classList.toggle("show-notfy-box");
+      notfyBox.classList.toggle("show-notfy-box");
+
 });
 
+*/
 
+//------New code for notification (if user click outside close the notification box)
+
+likeBtn.addEventListener("click",()=>{
+    let notfyBox = document.querySelector('.notification-box-close');
+      notfyBox.style.display="block";
+      window.addEventListener('mouseup',function(event){
+        if(event.target != notfyBox && event.target.parentNode != notfyBox){
+            notfyBox.style.display = 'none';
+        }
+  });  
+  
+  
+});
 
 // massages open
 
